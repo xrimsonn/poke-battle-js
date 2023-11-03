139d398b84615a3847d67c63fc8f76e412eef35e
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-02 20:46:04 -0600

    comments added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Implementación de una pila
 class Stack {
   constructor() {
     this.items = [];
@@ -17,6 +18,7 @@ class Stack {
   }
 }
 
+// Implementación de una cola
 class Queue {
   constructor() {
     this.items = [];
@@ -36,6 +38,7 @@ class Queue {
   }
 }
 
+// Inicialización de las variables
 let url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=150';
 let pokemonList = [];
 let battleHistory = new Stack();
@@ -43,26 +46,33 @@ let pokemonOponents = new Queue();
 let pokemonName = '';
 let pokemonURL = '';
 
+// Función para capitalizar la primera letra de una palabra
 function capitalizeFirstLetter(string) {
   return string
     ? string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
     : '';
 }
 
+// Función para guardar los nombres y URL's de los pokemones de la API
 async function fetchPokeAPI(url) {
+  // Obtenemos la respuesta
   const response = await fetch(url);
+  // La convertimos a formato JSON
   const data = await response.json();
+  // Guardamos todos los pokemones en la lista
   data.results.forEach((pokemon) => {
     pokemonList.push(pokemon);
   });
 }
 
+// Función para devolver los detalles de un pokemon especifico con su URL
 async function fetchPokemon(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
+// Función para calcular el total de los stats de un pokemon
 function calculateTotalStats(pokemon) {
   let totalStats = 0;
   pokemon.stats.forEach((stat) => {
@@ -71,18 +81,22 @@ function calculateTotalStats(pokemon) {
   return totalStats;
 }
 
+// Función para cargar los pokemones en el select
 async function selectPokemon() {
   await fetchPokeAPI(url);
   let select = document.getElementById('pokemonSelect');
   pokemonList.forEach((pokemon) => {
     let option = document.createElement('option');
-    option.value = capitalizeFirstLetter(pokemon.name);
+    // Le asignamos el texto y el valor a cada opcion del select
+    option.value = pokemon.name;
     option.text = capitalizeFirstLetter(pokemon.name);
     select.appendChild(option);
   });
 }
 
+// Función para seleccionar los rivales
 function selectOponents() {
+  // Cola de rivales a enfrentar
   pokemonOponents = new Queue();
   for (let i = 1; i <= 5; i++) {
     let randPokemon = Math.floor(Math.random() * 150);
@@ -91,35 +105,39 @@ function selectOponents() {
   }
 }
 
+// Función para obtener el pokemon seleccionado
 function getPokemon() {
   let pokemonName = document.getElementById('pokemonSelect').value;
   startBattles(pokemonName);
 }
 
+// Función para iniciar las batallas
 async function startBattles(pokemonName) {
   let section = document.getElementsByTagName('section')[0];
   let winner = '';
 
+  // Buscamos la URL del pokemon introducido
   pokemonURL = pokemonList.find((pokemon) => {
-    return pokemon.name === pokemonName.toLowerCase();
+    return pokemon.name === pokemonName;
   }).url;
 
   if (pokemonOponents.size() > 0) {
-    // Si la cola esta vacia, descolamos el primer elemento
+    // Si la cola no esta vacia, descolamos el primer elemento
     let oponent = pokemonOponents.dequeue();
 
+    // Obtenemos los detalles de los pokemones
     const oponentPokemon = await fetchPokemon(oponent.url);
     const oponentStats = calculateTotalStats(oponentPokemon);
-
     const selectedPokemon = await fetchPokemon(pokemonURL);
     const selectedStats = calculateTotalStats(selectedPokemon);
 
+    // Comparamos los stats de los pokemones para determinar al ganador
     winner =
       selectedStats > oponentStats ? selectedPokemon.name : oponentPokemon.name;
-
     let title = document.createElement('h3');
     title.innerText = 'Rival: ' + capitalizeFirstLetter(oponent.name);
 
+    // Creamos los elementos para mostrar los pokemones
     let pokemonSprite = document.createElement('img');
     pokemonSprite.src = selectedPokemon.sprites.front_default;
     let bolt = document.createElement('i');
@@ -127,26 +145,29 @@ async function startBattles(pokemonName) {
     let oponentSprite = document.createElement('img');
     oponentSprite.src = oponentPokemon.sprites.front_default;
 
+    // Creamos el elemento para mostrar el ganador
     let strong = document.createElement('strong');
     strong.innerHTML =
       winner.toLowerCase() === pokemonName.toLowerCase()
         ? '<ins>Ganador: ' + capitalizeFirstLetter(winner) + '</ins>'
         : '<del>Ganador: ' + capitalizeFirstLetter(winner) + '</del>';
 
+    // Creamos los botones para la siguiente batalla y para ver el ultimo resultado
     let nextButton = document.createElement('button');
     nextButton.innerHTML = 'Siguiente batalla';
     nextButton.className = 'contrast';
-
     let lastButton = document.createElement('button');
     lastButton.innerHTML = 'Ver ultimo resultado';
     lastButton.className = 'contrast';
 
+    // Creamos el div que contendra los pokemones
     let div = document.createElement('div');
     div.className = 'versus';
     div.appendChild(pokemonSprite);
     div.appendChild(bolt);
     div.appendChild(oponentSprite);
 
+    // Limpiamos el section y agregamos los elementos
     section.innerHTML = '';
     section.appendChild(title);
     section.appendChild(div);
@@ -166,19 +187,26 @@ async function startBattles(pokemonName) {
       section.appendChild(lastButton);
     }
 
+    // Eventos de los botones
     nextButton.onclick = () => {
+      // Guardamos el resultado de la batalla en la pila
       battleHistory.push(strong.innerHTML);
+      // Iniciamos la siguiente batalla
       startBattles(pokemonName);
     };
+
     lastButton.onclick = () => {
+      // Mostramos el ultimo resultado
       let prevStrong = document.createElement('strong');
       prevStrong.innerHTML = battleHistory.pop(); 
       section.append(prevStrong);
+      // Eliminamos el boton de ver ultimo resultado
       section.removeChild(lastButton);
     };
   }
 }
 
+// Función para cargar las demas funciones
 async function load() {
   await selectPokemon();
   selectOponents();
